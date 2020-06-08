68db593a4aca652d410ed76baccf74e8935df416
Cristhian Silva <crisilcart@gmail.com>
2020-06-08 10:50:55 -0500

    Squashed commit of the following:
    commit 59e36bb70c96640190414e9cab40558c56e3297f
    Author: Cristhian Silva <crisilcart@gmail.com>
    Date:   Mon Jun 8 10:49:24 2020 -0500
    
        javaFile.java Ready!!
    
    commit 3fbdeb759ed85b6b8ccfc61001fca08e179ce08d
    Author: Cristhian Silva <crisilcart@gmail.com>
    Date:   Mon Jun 8 10:47:49 2020 -0500
    
        javaFile.java file corrected

--- javaFile.java
+++ javaFile.java
@@ -1,4 +1,20 @@
-This is a NEW java file
-THIS IS ALSO WRONG WITH THE EXTENSION 
-i will try to correct the file extension
-this is for another commit
+// Create a commit on the feature2 branch. Add the string "feature 2 wip" to fileA.txt .
+// The commit message should be "add feature 2 wip".
+// 3. Create another commit on the feature2 branch. In fileA.txt , change the line "feature
+// 2 wip" to "feature 2". The commit message should be "add feature 2".
+
+
+public class Factorial {
+
+    public static void main(String[] args) {
+
+        int num = 10;
+        long factorial = 1;
+        for(int i = 1; i <= num; ++i)
+        {
+            // factorial = factorial * i;
+            factorial *= i;
+        }
+        System.out.printf("Factorial of %d = %d", num, factorial);
+    }
+}
